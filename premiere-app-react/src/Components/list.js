72907244b86aaecfecb834fc8d9eed75241fcd6d
Franck Garcia <franck.garcia@cfa-afti.fr>
2020-11-02 16:05:27 +0100

    first commit

---
+++ premiere-app-react/src/Components/list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class List extends React.Component {   
+    render () {
+        return <ul>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Google</li>
+        </ul>
+    }
+}
